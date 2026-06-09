2705da675da59dc294e11daf6387a2b4b849c999
U0105 <hosseinabbasi80.ha@gmail.com>
2026-06-09 09:37:59 +0330

    class based api's.
    + post list class added.
    + post detail class added.
    + put , post , delete methodes migrated to class based views.
    
    overall:
        in this section we learned about class based api's
        and migrated all of our api function to class base.

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import postlist, postdetail
+from blog.api.v1 import views
 app_name = "api-v1"
 
 urlpatterns = [
-    path("posts/",postlist,name="post-list"),
-    path("posts/<int:id>/",postdetail,name="post-detail"),
+    # path("posts/",postlist,name="post-list"),
+    path("posts/",views.PostList.as_view(),name="post-list"),
+    # path("posts/<int:id>/",views.postdetail,name="post-detail"),
+    path("posts/<int:id>/",views.PostDetail.as_view(),name="post-detail"),
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from rest_framework.views import APIView
+
+'''
 @api_view(["GET","POST"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def postlist(request):
@@ -18,7 +21,34 @@ def postlist(request):
         serialized.is_valid(raise_exception=True)
         serialized.save()
         return Response(serialized.data)
-        
+'''
+class PostList(APIView):
+    '''
+    this is the class for geting list of posts and making
+    new ones.
+    '''
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = Postserializer
+    def get(self,request):
+        '''
+        get method for getting post lists.
+        '''
+        posts = Post.objects.filter(status=True)
+        serialized = Postserializer(posts,many=True)
+        return Response(serialized.data)
+    
+    def post(self,request):
+        '''
+        post method for posting.
+
+        '''
+        serialized = Postserializer(data=request.data)
+        serialized.is_valid(raise_exception=True)
+        serialized.save()
+        return Response(serialized.data)
+
+'''
 @api_view(["GET","PUT","DELETE"])
 def postdetail(request,id):
     post = get_object_or_404(Post,pk=id,status=True)
@@ -31,5 +61,35 @@ def postdetail(request,id):
         serialized.save()
         return Response(serialized.data)
     elif request.method == "DELETE":
+        post.delete()
+        return Response({" detail": "Post Deleted Successfully."},status=status.HTTP_204_NO_CONTENT)
+'''
+
+class PostDetail(APIView):
+
+    '''
+    geting details of a post and updating it and removing it.
+    '''
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = Postserializer
+
+
+    def get(self,request,id):
+
+        post = get_object_or_404(Post,pk=id,status=True)
+        serialized = self.serializer_class(post)
+        return Response(serialized.data)
+
+    def put(self,request,id):
+
+        post = get_object_or_404(Post,pk=id,status=True)
+        serialized = self.serializer_class(post,data=request.data)
+        serialized.is_valid(raise_exception=True)
+        serialized.save()
+        return Response(serialized.data)
+    
+    def delete(self,request,id):
+
+        post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({" detail": "Post Deleted Successfully."},status=status.HTTP_204_NO_CONTENT)
